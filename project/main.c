ec7308863211b6450437e232e32f388daa3726c2
Daniel <145384176+Daniel16Bit@users.noreply.github.com>
2025-10-24 00:55:48 -0300

    !

--- project/main.c
+++ project/main.c
@@ -3,31 +3,57 @@
 #include <windows.h>
 #include <stdio.h>
 
+int main()
+{
 
-int main (){
-
+    // LEMBRETE: Variáveis da Winsock
     WSADATA wsa;
     int result;
+
+    // LEMBBRETE: Variáveis do socket e rede
     SOCKET servidor;
+    struct sockaddr_in endereco;
+    int resultado_bind;
+    int resultado_listen;
 
-    result = WSAStartup(MAKEWORD(2,2), &wsa);
+    result = WSAStartup(MAKEWORD(2, 2), &wsa);
 
-    if(result != 0){
+    if (result != 0){
         printf("Erro ao inicializar o Winsock. Codigo: %d\n", result);
         return 1;
     }
 
     servidor = socket(AF_INET, SOCK_STREAM, 0);
-    if(servidor == INVALID_SOCKET){
+    if (servidor == INVALID_SOCKET){
         printf("Erro ao inicializar o Socket. Codigo: %d\n", WSAGetLastError());
         WSACleanup();
         return 1;
     }
 
+    endereco.sin_family = AF_INET;         // Protocolo IPV4
+    endereco.sin_port = htons(8080);       // Converte a porta 8080 para FORMATO de rede (big-endian)
+    endereco.sin_addr.s_addr = INADDR_ANY; // Aceita que qlq IP faça conexão no servidor
+
+    resultado_bind = bind(servidor, (struct sockaddr*)&endereco, sizeof(endereco));
+    if (resultado_bind == SOCKET_ERROR){
+        printf("Erro ao fazer o BIND. Codigo de error: %d\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+
+    resultado_listen = listen(servidor, 5);
+    if (resultado_listen == SOCKET_ERROR){
+        printf("Erro ao colocar socket em modo listen. Codigo: %d\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+
+    
     printf("Socket Criado com sucesso!\n");
+    printf("Servidor escutando na porta 8080...\n");
     printf("Pressione ENTER para sair...");
     getchar();
-    
+
     WSACleanup();
     return 0;
 }
